8fd3f6589d06efcbc2b64c207f5ca758efc45b68
Andrew VanDamme <andrew.vandamme@emc.com>
2017-05-13 14:36:44 -0400

    initial VC layout

---
+++ Soundy Sounds/SoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SoundViewController.swift
+//  Soundy Sounds
+//
+//  Created by Andrew VanDamme on 5/13/17.
+//  Copyright © 2017 Andrew VanDamme. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+
+    @IBAction func playTapped(_ sender: Any) {
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
