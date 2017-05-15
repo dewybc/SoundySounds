0f7947c310c38bd31c962d9ec24341e478eb73ee
Andrew VanDamme <andrew.vandamme@emc.com>
2017-05-14 22:18:03 -0400

    final prod

--- Soundy Sounds/SoundViewController.swift
+++ Soundy Sounds/SoundViewController.swift
@@ -10,26 +10,104 @@ import UIKit
 import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder?
+    var audioPlayer : AVAudioPlayer?
+    var audioURL : URL?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        setupRecorder()
+        playButton.isEnabled = false
+        addButton.isEnabled = false
     }
     
+    func setupRecorder() {
+        do {
+            //Create an audio session, for playing and recording
+            
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            //force audio to go through the speakers on the device
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            
+            //create url for the audio files to be saved to
+            
+            let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basePath, "audio.m4a"]
+            audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
+
+            
+            //Create settings for audio recorder below
+            
+            var settings : [String:Any] = [:]
+            settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
+            settings[AVSampleRateKey] = 44100.0
+            settings[AVNumberOfChannelsKey] = 2
+            
+            
+            //Create audio recorder object
+            
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings: settings)
+            audioRecorder!.prepareToRecord()
+            
+        } catch let error as NSError {
+            print(error)
+        }
+    }
+    
+    
+    
     @IBAction func recordTapped(_ sender: Any) {
-    }
+        
+        if audioRecorder!.isRecording {
+            //Stop the recording
+            audioRecorder?.stop()
+            
+            //Change button title to Record
+            recordButton.setTitle("Record", for: .normal)
+            
+            playButton.isEnabled = true
+            
+        } else {
+            //Start the recording
+            audioRecorder?.record()
 
+            //Change button title to Stop
+            recordButton.setTitle("Stop", for: .normal)
+            addButton.isEnabled = true
+
+        }
+        
+    }
+    
     @IBAction func playTapped(_ sender: Any) {
+        do {
+        try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
+            audioPlayer!.play()
+        } catch {
+            
+        }
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let sound = Sound(context: context)
+        sound.name = textField.text
+        sound.audio = NSData(contentsOf: audioURL!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+        
+        
     }
+    
 }
+
